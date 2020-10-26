5b515131fc63bd19de183d131e747be708928960
Leon Bogaert <leon@tim-online.nl>
2020-10-26 12:44:39 +0100

    First validation version

--- finvoice.go
+++ finvoice.go
@@ -1,7 +1,11 @@
 package finvoice
 
 import (
+	"encoding/json"
 	"encoding/xml"
+	"fmt"
+	"strings"
+	"time"
 
 	"github.com/cydev/zero"
 	"github.com/omniboost/go-finvoice/omitempty"
@@ -10,17 +14,17 @@ import (
 type Finvoice struct {
 	XMLName                    xml.Name                   `xml:"Finvoice"`
 	Version                    string                     `xml:"Version,attr"`
-	Xsi                        string                     `xml:"xsi,attr"`
-	NoNamespaceSchemaLocation  string                     `xml:"noNamespaceSchemaLocation,attr"`
-	MessageTransmissionDetails MessageTransmissionDetails `xml:"MessageTransmissionDetails,omitempty"`
-	SellerPartyDetails         SellerPartyDetails         `xml:"SellerPartyDetails,omitempty"`
-	SellerInformationDetails   SellerInformationDetails   `xml:"SellerInformationDetails,omitempty"`
-	BuyerPartyDetails          BuyerPartyDetails          `xml:"BuyerPartyDetails,omitempty"`
-	DeliveryDetails            DeliveryDetails            `xml:"DeliveryDetails,omitempty"`
-	InvoiceDetails             InvoiceDetails             `xml:"InvoiceDetails,omitempty"`
-	PaymentStatusDetails       PaymentStatusDetails       `xml:"PaymentStatusDetails,omitempty"`
+	Xsi                        string                     `xml:"xmlns:xsi,attr"`
+	NoNamespaceSchemaLocation  string                     `xml:"xsi:noNamespaceSchemaLocation,attr"`
+	MessageTransmissionDetails MessageTransmissionDetails `xml:"MessageTransmissionDetails"`
+	SellerPartyDetails         SellerPartyDetails         `xml:"SellerPartyDetails"`
+	SellerInformationDetails   SellerInformationDetails   `xml:"SellerInformationDetails"`
+	BuyerPartyDetails          BuyerPartyDetails          `xml:"BuyerPartyDetails"`
+	DeliveryDetails            DeliveryDetails            `xml:"DeliveryDetails"`
+	InvoiceDetails             InvoiceDetails             `xml:"InvoiceDetails"`
+	PaymentStatusDetails       PaymentStatusDetails       `xml:"PaymentStatusDetails"`
 	InvoiceRow                 []InvoiceRow               `xml:"InvoiceRow"`
-	EpiDetails                 EpiDetails                 `xml:"EpiDetails,omitempty"`
+	EpiDetails                 EpiDetails                 `xml:"EpiDetails"`
 }
 
 func (f Finvoice) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
@@ -29,8 +33,9 @@ func (f Finvoice) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 
 func NewFinvoice() *Finvoice {
 	return &Finvoice{
+		Version:                   "3.0",
 		Xsi:                       "http://www.w3.org/2001/XMLSchema-instance",
-		NoNamespaceSchemaLocation: "Finvoice2.01.xsd",
+		NoNamespaceSchemaLocation: "Finvoice3.0.xsd",
 	}
 }
 
@@ -40,9 +45,17 @@ type MessageTransmissionDetails struct {
 	MessageDetails         MessageDetails         `xml:"MessageDetails"`
 }
 
+func (m MessageTransmissionDetails) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return omitempty.MarshalXML(m, e, start)
+}
+
 type MessageSenderDetails struct {
 	FromIdentifier    string `xml:"FromIdentifier"`
 	FromIntermediator string `xml:"FromIntermediator"`
+}
+
+func (m MessageSenderDetails) IsEmpty() bool {
+	return zero.IsZero(m)
 }
 
 type MessageReceiverDetails struct {
@@ -51,9 +64,9 @@ type MessageReceiverDetails struct {
 }
 
 type MessageDetails struct {
-	MessageIdentifier      string `xml:"MessageIdentifier"`
-	MessageTimeStamp       string `xml:"MessageTimeStamp"`
-	RefToMessageIdentifier string `xml:"RefToMessageIdentifier"`
+	MessageIdentifier      string   `xml:"MessageIdentifier"`
+	MessageTimeStamp       DateTime `xml:"MessageTimeStamp"`
+	RefToMessageIdentifier string   `xml:"RefToMessageIdentifier"`
 }
 
 type SellerPartyDetails struct {
@@ -105,38 +118,25 @@ type BuyerPostalAddressDetails struct {
 }
 
 type DeliveryDetails struct {
-	DeliveryDate struct {
-		Format string `xml:"Format,attr"`
-	} `xml:"DeliveryDate"`
+	DeliveryDate Date `xml:"DeliveryDate"`
 }
 
 type InvoiceDetails struct {
-	InvoiceTypeCode string `xml:"InvoiceTypeCode"`
-	InvoiceTypeText string `xml:"InvoiceTypeText"`
-	OriginCode      string `xml:"OriginCode"`
-	InvoiceNumber   string `xml:"InvoiceNumber"`
-	InvoiceDate     struct {
-		Format string `xml:"Format,attr"`
-	} `xml:"InvoiceDate"`
-	OrderIdentifier               string         `xml:"OrderIdentifier"`
-	InvoiceTotalVATExcludedAmount AmountCurrency `xml:"InvoiceTotalVatExcludedAmount"`
-	InvoiceTotalVatAmount         AmountCurrency `xml:"InvoiceTotalVatAmount"`
-	InvoiceTotalVatIncludedAmount AmountCurrency `xml:"InvoiceTotalVatIncludedAmount"`
-	VATSpecificationDetails       struct {
-		VATBaseAmount  AmountCurrency `xml:"VatBaseAmount"`
-		VATRatePercent string         `xml:"VatRatePercent"`
-		VATRateAmount  AmountCurrency `xml:"VatRateAmount"`
-	} `xml:"VatSpecificationDetails"`
-	PaymentTermsDetails struct {
-		PaymentTermsFreeText string `xml:"PaymentTermsFreeText"`
-		InvoiceDueDate       struct {
-			Format string `xml:"Format,attr"`
-		} `xml:"InvoiceDueDate"`
-		PaymentOverDueFineDetails struct {
-			PaymentOverDueFineFreeText string `xml:"PaymentOverDueFineFreeText"`
-			PaymentOverDueFinePercent  string `xml:"PaymentOverDueFinePercent"`
-		} `xml:"PaymentOverDueFineDetails"`
-	} `xml:"PaymentTermsDetails"`
+	InvoiceTypeCode               string                  `xml:"InvoiceTypeCode"`
+	InvoiceTypeText               string                  `xml:"InvoiceTypeText"`
+	OriginCode                    string                  `xml:"OriginCode"`
+	InvoiceNumber                 string                  `xml:"InvoiceNumber"`
+	InvoiceDate                   Date                    `xml:"InvoiceDate"`
+	SellerReferenceIdentifier     string                  `xml:"SellerReferenceIdentifier"`
+	BuyersSellerIdentifier        string                  `xml:"BuyersSellerIdentifier"`
+	SellersBuyerIdentifier        string                  `xml:"SellersBuyerIdentifier"`
+	BuyerReferenceIdentifier      string                  `xml:"BuyerReferenceIdentifier"`
+	OrderIdentifier               string                  `xml:"OrderIdentifier"`
+	InvoiceTotalVATExcludedAmount AmountCurrency          `xml:"InvoiceTotalVatExcludedAmount"`
+	InvoiceTotalVatAmount         AmountCurrency          `xml:"InvoiceTotalVatAmount"`
+	InvoiceTotalVatIncludedAmount AmountCurrency          `xml:"InvoiceTotalVatIncludedAmount"`
+	VATSpecificationDetails       VATSpecificationDetails `xml:"VatSpecificationDetails"`
+	PaymentTermsDetails           PaymentTermsDetails     `xml:"PaymentTermsDetails"`
 }
 
 func (i InvoiceDetails) IsEmpty() bool {
@@ -144,7 +144,11 @@ func (i InvoiceDetails) IsEmpty() bool {
 }
 
 type PaymentStatusDetails struct {
-	PaymentStatusCode string `xml:"PaymentStatusCode"`
+	PaymentStatusCode string `xml:"PaymentStatusCode,omitempty"`
+}
+
+func (p PaymentStatusDetails) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return omitempty.MarshalXML(p, e, start)
 }
 
 func (p PaymentStatusDetails) IsEmpty() bool {
@@ -164,50 +168,165 @@ type InvoiceRow struct {
 	RowVATExcludedAmount AmountCurrency `xml:"RowVatExcludedAmount"`
 }
 
+func (i InvoiceRow) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return omitempty.MarshalXML(i, e, start)
+}
+
 type InvoicedQuantity struct {
 	QuantityUnitCode string `xml:"QuantityUnitCode,attr"`
-	Amount           Amount `xml:",attr"`
+	Amount           Amount `xml:",chardata"`
+}
+
+func (i InvoicedQuantity) IsEmpty() bool {
+	return zero.IsZero(i)
 }
 
 type EpiDetails struct {
-	EpiIdentificationDetails struct {
-		EpiDate struct {
-			Format string `xml:"Format,attr"`
-		} `xml:"EpiDate"`
-		EpiReference string `xml:"EpiReference"`
-	} `xml:"EpiIdentificationDetails"`
-	EpiPartyDetails struct {
-		EpiBfiPartyDetails struct {
-			EpiBfiIdentifier struct {
-				IdentificationSchemeName string `xml:"IdentificationSchemeName,attr"`
-			} `xml:"EpiBfiIdentifier"`
-		} `xml:"EpiBfiPartyDetails"`
-		EpiBeneficiaryPartyDetails struct {
-			EpiNameAddressDetails string `xml:"EpiNameAddressDetails"`
-			EpiBei                string `xml:"EpiBei"`
-			EpiAccountID          struct {
-				IdentificationSchemeName string `xml:"IdentificationSchemeName,attr"`
-			} `xml:"EpiAccountID"`
-		} `xml:"EpiBeneficiaryPartyDetails"`
-	} `xml:"EpiPartyDetails"`
-	EpiPaymentInstructionDetails struct {
-		EpiPaymentInstructionId     string `xml:"EpiPaymentInstructionId"`
-		EpiRemittanceInfoIdentifier struct {
-			IdentificationSchemeName string `xml:"IdentificationSchemeName,attr"`
-		} `xml:"EpiRemittanceInfoIdentifier"`
-		EpiInstructedAmount AmountCurrency `xml:"EpiInstructedAmount"`
-		EpiCharge           struct {
-			ChargeOption string `xml:"ChargeOption,attr"`
-		} `xml:"EpiCharge"`
-		EpiDateOptionDate struct {
-			Format string `xml:"Format,attr"`
-		} `xml:"EpiDateOptionDate"`
-	} `xml:"EpiPaymentInstructionDetails"`
+	EpiIdentificationDetails     EpiIdentificationDetails     `xml:"EpiIdentificationDetails"`
+	EpiPartyDetails              EpiPartyDetails              `xml:"EpiPartyDetails"`
+	EpiPaymentInstructionDetails EpiPaymentInstructionDetails `xml:"EpiPaymentInstructionDetails"`
+}
+
+func (d EpiDetails) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return omitempty.MarshalXML(d, e, start)
+}
+
+type EpiIdentificationDetails struct {
+	EpiDate      Date   `xml:"EpiDate"`
+	EpiReference string `xml:"EpiReference"`
+}
+
+func (d EpiIdentificationDetails) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return omitempty.MarshalXML(d, e, start)
+}
+
+type EpiPartyDetails struct {
+	EpiBfiPartyDetails         EpiBfiPartyDetails         `xml:"EpiBfiPartyDetails"`
+	EpiBeneficiaryPartyDetails EpiBeneficiaryPartyDetails `xml:"EpiBeneficiaryPartyDetails"`
+}
+
+type EpiBfiPartyDetails struct {
+	EpiBfiIdentifier EpiBfiIdentifier `xml:"EpiBfiIdentifier"`
+}
+
+type EpiBfiIdentifier struct {
+	IdentificationSchemeName string `xml:"IdentificationSchemeName,attr"`
+	Value                    string `xml:",chardata"`
+}
+
+type EpiBeneficiaryPartyDetails struct {
+	EpiNameAddressDetails string       `xml:"EpiNameAddressDetails"`
+	EpiBei                string       `xml:"EpiBei"`
+	EpiAccountID          EpiAccountID `xml:"EpiAccountID"`
+}
+
+type EpiAccountID struct {
+	IdentificationSchemeName string `xml:"IdentificationSchemeName,attr"`
+	Value                    string `xml:",chardata"`
+}
+
+type EpiPaymentInstructionDetails struct {
+	EpiPaymentInstructionId     string                      `xml:"EpiPaymentInstructionId"`
+	EpiRemittanceInfoIdentifier EpiRemittanceInfoIdentifier `xml:"EpiRemittanceInfoIdentifier"`
+	EpiInstructedAmount         AmountCurrency              `xml:"EpiInstructedAmount"`
+	EpiCharge                   EpiCharge                   `xml:"EpiCharge"`
+	EpiDateOptionDate           Date                        `xml:"EpiDateOptionDate"`
+}
+
+type EpiRemittanceInfoIdentifier struct {
+	IdentificationSchemeName string `xml:"IdentificationSchemeName,attr"`
+	Value                    string `xml:",chardata"`
+}
+
+type EpiCharge struct {
+	ChargeOption string `xml:"ChargeOption,attr"`
 }
 
 type Amount float64
 
+func (a Amount) MarshalText() ([]byte, error) {
+	s := fmt.Sprintf("%.2f", float64(a))
+	s = strings.Replace(s, ".", ",", -1)
+	return []byte(s), nil
+}
+
 type AmountCurrency struct {
 	AmountCurrencyIdentifier string `xml:"AmountCurrencyIdentifier,attr"`
-	Amount                   Amount `xml:",attr"`
+	Amount                   Amount `xml:",chardata"`
+}
+
+func (d AmountCurrency) IsEmpty() bool {
+	return d.AmountCurrencyIdentifier == ""
+}
+
+type Date struct {
+	Format string `xml:"Format,attr"`
+	Date   string `xml:",chardata"`
+}
+
+func (d Date) IsEmpty() bool {
+	return zero.IsZero(d)
+}
+
+type Number float64
+
+func (n Number) IsEmpty() bool {
+	return n == Number(0.0)
+}
+
+type VATSpecificationDetails struct {
+	VATBaseAmount  AmountCurrency `xml:"VatBaseAmount"`
+	VATRatePercent string         `xml:"VatRatePercent"`
+	VATRateAmount  AmountCurrency `xml:"VatRateAmount"`
+}
+
+type PaymentTermsDetails struct {
+	PaymentTermsFreeText      string `xml:"PaymentTermsFreeText"`
+	InvoiceDueDate            Date   `xml:"InvoiceDueDate,omitempty"`
+	PaymentOverDueFineDetails struct {
+		PaymentOverDueFineFreeText string `xml:"PaymentOverDueFineFreeText"`
+		PaymentOverDueFinePercent  Number `xml:"PaymentOverDueFinePercent,omitempty"`
+	} `xml:"PaymentOverDueFineDetails"`
+}
+
+func (p PaymentTermsDetails) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return omitempty.MarshalXML(p, e, start)
+}
+
+type DateTime struct {
+	time.Time
+}
+
+func (d *DateTime) MarshalJSON() ([]byte, error) {
+	if d.Time.IsZero() {
+		return json.Marshal(nil)
+	}
+
+	return json.Marshal(d.Time.Format("2006-01-02T15:04:05-07:00"))
+}
+
+func (d *DateTime) UnmarshalJSON(text []byte) (err error) {
+	var value string
+	err = json.Unmarshal(text, &value)
+	if err != nil {
+		return err
+	}
+
+	if value == "" {
+		return nil
+	}
+
+	// first try standard date
+	d.Time, err = time.Parse(time.RFC3339, value)
+	if err == nil {
+		return nil
+	}
+
+	// try untill date format
+	d.Time, err = time.Parse("2006-01-02T15:04:05-07:00", value)
+	return
+}
+
+func (d DateTime) MarshalText() ([]byte, error) {
+	return []byte(d.Format("2006-01-02T15:04:05-07:00")), nil
 }
--- finvoice_test.go
+++ finvoice_test.go
@@ -2,16 +2,101 @@ package finvoice_test
 
 import (
 	"encoding/xml"
-	"log"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/omniboost/go-finvoice"
 )
 
 func TestFinvoice(t *testing.T) {
 	f := finvoice.NewFinvoice()
+	f.MessageTransmissionDetails = finvoice.MessageTransmissionDetails{
+		MessageSenderDetails: finvoice.MessageSenderDetails{
+			FromIdentifier:    "From Identifier",
+			FromIntermediator: "From Intermediator",
+		},
+		MessageReceiverDetails: finvoice.MessageReceiverDetails{
+			ToIdentifier:    "To Identifier",
+			ToIntermediator: "To Intermediator",
+		},
+		MessageDetails: finvoice.MessageDetails{
+			MessageIdentifier:      "Message Identifier",
+			MessageTimeStamp:       finvoice.DateTime{time.Now()},
+			RefToMessageIdentifier: "",
+		},
+	}
+	f.SellerPartyDetails = finvoice.SellerPartyDetails{
+		SellerPartyIdentifier:  "9876543-0",
+		SellerOrganisationName: []string{"Seller Organisation Name"},
+		SellerPostalAddressDetails: finvoice.SellerPostalAddressDetails{
+			SellerStreetName:         "TEST",
+			SellerTownName:           "TEST",
+			SellerPostCodeIdentifier: "TEST",
+			CountryCode:              "NL",
+		},
+	}
+	f.BuyerPartyDetails = finvoice.BuyerPartyDetails{
+		BuyerPartyIdentifier:        "Buyer Party Identifier",
+		BuyerOrganisationName:       "Omniboost B.V.",
+		BuyerOrganisationDepartment: []string{},
+		BuyerOrganisationTaxCode:    "",
+		BuyerPostalAddressDetails: finvoice.BuyerPostalAddressDetails{
+			BuyerStreetName:              "Stadhuisplein 3",
+			BuyerTownName:                "Terneuzen",
+			BuyerPostCodeIdentifier:      "4531GZ",
+			CountryCode:                  "NL",
+			CountryName:                  "Netherlands",
+			BuyerPostOfficeBoxIdentifier: "",
+		},
+	}
+	f.DeliveryDetails = finvoice.DeliveryDetails{
+		DeliveryDate: finvoice.Date{
+			Format: "CCYYMMDD",
+			Date:   "19820412",
+		},
+	}
+	f.InvoiceDetails = finvoice.InvoiceDetails{
+		InvoiceTypeCode: "INV01",
+		InvoiceTypeText: "Invoice",
+		OriginCode:      "Original",
+		InvoiceNumber:   "69",
+		InvoiceDate: finvoice.Date{
+			Format: "CCYYMMDD",
+			Date:   "19820412",
+		},
+		SellerReferenceIdentifier: "Seller Reference Identifier",
+		BuyersSellerIdentifier:    "Buyers Seller Identifier",
+		SellersBuyerIdentifier:    "Sellers Buyer Identifier",
+		OrderIdentifier:           "Order Identifier",
+		BuyerReferenceIdentifier:  "Buyer Reference Identifier",
+		InvoiceTotalVATExcludedAmount: finvoice.AmountCurrency{
+			AmountCurrencyIdentifier: "EUR",
+			Amount:                   12.0,
+		},
+		InvoiceTotalVatAmount: finvoice.AmountCurrency{
+			AmountCurrencyIdentifier: "EUR",
+			Amount:                   12.0,
+		},
+		InvoiceTotalVatIncludedAmount: finvoice.AmountCurrency{
+			AmountCurrencyIdentifier: "EUR",
+			Amount:                   12.0,
+		},
+		VATSpecificationDetails: finvoice.VATSpecificationDetails{
+			VATBaseAmount: finvoice.AmountCurrency{
+				AmountCurrencyIdentifier: "EUR",
+				Amount:                   12.0,
+			},
+			VATRatePercent: "12",
+			VATRateAmount: finvoice.AmountCurrency{
+				AmountCurrencyIdentifier: "EUR",
+				Amount:                   12.0,
+			},
+		},
+	}
 	f.InvoiceRow = append(f.InvoiceRow, finvoice.InvoiceRow{
-		ArticleName: "Test row",
+		ArticleName:       "Test row",
+		ArticleIdentifier: "Article Identifier",
 		InvoicedQuantity: finvoice.InvoicedQuantity{
 			QuantityUnitCode: "EUR",
 			Amount:           finvoice.Amount(121.0),
@@ -25,10 +110,51 @@ func TestFinvoice(t *testing.T) {
 			Amount:                   finvoice.Amount(100.0),
 		},
 	})
+	f.EpiDetails = finvoice.EpiDetails{
+		EpiIdentificationDetails: finvoice.EpiIdentificationDetails{
+			EpiDate: finvoice.Date{
+				Format: "CCYYMMDD",
+				Date:   "19820412",
+			},
+		},
+		EpiPartyDetails: finvoice.EpiPartyDetails{
+			EpiBfiPartyDetails: finvoice.EpiBfiPartyDetails{
+				EpiBfiIdentifier: finvoice.EpiBfiIdentifier{
+					IdentificationSchemeName: "BIC",
+					Value:                    "BANKFIHH",
+				},
+			},
+			EpiBeneficiaryPartyDetails: finvoice.EpiBeneficiaryPartyDetails{
+				EpiNameAddressDetails: "TEST",
+				EpiBei:                "5647382910",
+				EpiAccountID: finvoice.EpiAccountID{
+					IdentificationSchemeName: "IBAN",
+					Value:                    "FI2757800750155448",
+				},
+			},
+		},
+		EpiPaymentInstructionDetails: finvoice.EpiPaymentInstructionDetails{
+			EpiRemittanceInfoIdentifier: finvoice.EpiRemittanceInfoIdentifier{
+				IdentificationSchemeName: "ISO",
+				Value:                    "RF471234567890",
+			},
+			EpiInstructedAmount: finvoice.AmountCurrency{
+				AmountCurrencyIdentifier: "EUR",
+				Amount:                   12.0,
+			},
+			EpiCharge: finvoice.EpiCharge{
+				ChargeOption: "SLEV",
+			},
+			EpiDateOptionDate: finvoice.Date{
+				Format: "CCYYMMDD",
+				Date:   "19820412",
+			},
+		},
+	}
 
 	b, err := xml.MarshalIndent(f, "", "  ")
 	if err != nil {
 		t.Fatal(err)
 	}
-	log.Println(string(b))
+	fmt.Println(string(b))
 }
